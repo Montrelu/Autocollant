f11348400cdc373fbfac76c58fa88a0b1f9df597
montrelu <noah.montreuil@gmail.com>
2023-04-02 12:14:54 -0300

    try 2 workflows

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using MongoDB.Driver;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = "mongodb+srv://noahmontreuil:Noah3157@tm.5pivxuf.mongodb.net/?retryWrites=true&w=majority";
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
